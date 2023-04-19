1b393f92d27276cfd75dc30ac99f867a40827c34
lethanhan <lethanhancr7112001@gmail.com>
2023-04-19 20:52:13 +0700

    socket

--- index.js
+++ index.js
@@ -1,64 +1,80 @@
-const express = require("express");
-const app = express();
-const path = require("path");
-const dotenv = require("dotenv");
-const helmet = require("helmet");
-const morgan = require("morgan");
-const multer = require("multer");
-const cors = require("cors");
-const db = require("./src/db/index");
-const authRoutes = require("./src/routes/authRoutes");
-const userRoutes = require("./src/routes/userRoute");
-const postRoutes = require("./src/routes/postRoute");
-const commentRoutes = require("./src/routes/commentRoutes");
-const messageRoutes = require("./src/routes/message");
-const conversationRoutes = require("./src/routes/conversation");
-
-dotenv.config();
-
-db.connect();
-
-app.use("/images", express.static(path.join(__dirname, "src/public/images")));
-app.use("/videos", express.static(path.join(__dirname, "src/public/videos")));
-
-app.use(cors());
-app.use(express.json({ limit: "200mb" }));
-app.use(
-  express.urlencoded({
-    limit: "200mb",
-    extended: true,
-  })
-);
-app.use(helmet());
-app.use(morgan("common"));
-
-const storage = multer.diskStorage({
-  destination: (req, file, cb) => {
-    cb(null, "src/public/images");
-  },
-  filename: (req, file, cb) => {
-    cb(null, req.body.name);
+const dotenv = require("dotenv").config();
+const io = require("socket.io")(process.env.SOCKET_SERVER || 8900, {
+  cors: {
+    origin: "http://localhost:3000",
   },
 });
 
-const upload = multer({ storage: storage });
-app.post("/api/upload", upload.single("file"), (req, res) => {
-  try {
-    return res.status(200).json("File uploded successfully");
-  } catch (error) {
-    console.error(error);
-  }
+let users = [];
+
+const addUser = (userId, socketId) => {
+  !users.some((user) => user.userId === userId) &&
+    users.push({ userId, socketId });
+};
+
+const removeUser = (socketId) => {
+  users = users.filter((user) => user.socketId !== socketId);
+};
+
+const getUsers = (userId) => {
+  return users.find((user) => user.userId === userId);
+};
+
+io.on("connection", (socket) => {
+  //when ceonnect
+  console.log("a user connected.");
+
+  //take userId and socketId from user
+  socket.on("addUser", (userId) => {
+    addUser(userId, socket.id);
+    io.emit("getUsers", users);
+  });
+
+  //send and get message
+  socket.on("sendMessage", ({ senderId, receiverId, text }) => {
+    const user = getUsers(receiverId);
+    io.to(user?.socketId).emit("getMessage", {
+      senderId,
+      text,
+    });
+  });
+
+  socket.on("sendBell", ({ senderName, receiverName, type }) => {
+    const user = getUsers(receiverName);
+    io.to(user?.socketId).emit("getBell", {
+      senderName,
+      type,
+    });
+  });
+
+  //when disconnect
+  socket.on("disconnect", () => {
+    console.log("a user disconnected!");
+    removeUser(socket.id);
+    io.emit("getUsers", users);
+  });
 });
 
-const PORT = process.env.PORT;
+io.on("connection", (socket) => {
+  //when ceonnect
+  console.log("a user connected.");
 
-app.use("/api/auth", authRoutes);
-app.use("/api/posts", postRoutes);
-app.use("/api/users", userRoutes);
-app.use("/api/comments", commentRoutes);
-app.use("/api/message", messageRoutes);
-app.use("/api/conversation", conversationRoutes);
+  //take userId and socketId from user
+  socket.on("addNewUser", (userId) => {
+    addUser(userId, socket.id);
+  });
 
-app.listen(process.env.PORT || 8000, () => {
-  console.log(`Backend server is running! is ${PORT}`);
+  socket.on("sendBell", ({ senderName, receiverName, type }) => {
+    const user = getUsers(receiverName);
+    io.to(user?.socketId).emit("getBell", {
+      senderName,
+      type,
+    });
+  });
+
+  //when disconnect
+  socket.on("disconnect", () => {
+    console.log("a user disconnected!");
+    removeUser(socket.id);
+  });
 });
